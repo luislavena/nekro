e3eb61d03f4cc012490d5d9e5269b49aa38da614
Luis Lavena <luislavena@gmail.com>
2008-10-19 16:34:52 -0300

    Simplified test example, cleanup Fibonacci.

--- test.rb
+++ test.rb
@@ -5,62 +5,6 @@ require 'unified_ruby'
 
 require 'pp'
 
-code =<<-SCRIPT
-  class Fib
-    def fib(n)
-      if n <= 1
-        return 1
-      else 
-        return fib(n-1) + fib(n-2)
-      end
-    end
-  end
-
-  puts Fib.fib(20)
-SCRIPT
-
-code2 =<<-SCRIPT
-  class Foo
-    def hello
-      puts "hello!"
-    end
-  end
-  
-  class Bar < Foo
-    def goodbye
-      puts "bye!"
-    end
-  end
-
-  Bar.new.hello()
-SCRIPT
-
-code3 =<<-SCRIPT
-  class Time
-    def new
-      @now = date_now()
-      return self
-    end
-
-    def to_i
-      return @now
-    end
-  end
-  
-  start_time = Time.new
-  puts start_time.to_i
-SCRIPT
-
-=begin
-class Foo
-  def hello(name)
-    $x = 100
-    puts "Hello World!"
-    puts $x
-  end
-end
-=end
-
 class Neko < SexpProcessor
   include UnifiedRuby
 
@@ -82,9 +26,62 @@ class Neko < SexpProcessor
     XLATE_MAP[fname] || fname
   end
 
+  def self.process(exp)
+    self.new.process(exp)
+  end
+
   def process(exp)
     exp = Sexp.from_array(exp) if Array === exp unless Sexp === exp
     super exp
+  end
+
+  def process_block(exp)
+    result = []
+
+    exp << nil if exp.empty?
+    until exp.empty? do
+      code = exp.shift
+      if code.nil? or code.first == :nil then
+        result << "# do nothing"
+      else
+        result << process(code)
+      end
+    end
+
+    result = result.join "\n"
+
+    result = case self.context[1]
+             when nil, :scope, :if, :iter, :resbody, :when, :while then
+               result + "\n"
+             else
+               "(#{result})"
+             end
+
+    return result
+  end
+
+  def process_defn(exp)
+    type1 = exp[1].first
+    #type2 = exp[2] rescue nil #.first 
+
+    result = []
+
+    case type1
+        when :scope, :args then
+        name = exp.shift.to_s
+        name.gsub!('?', '_q')
+        args = process(exp.shift)
+        body = process(exp.shift)
+        parent = @klasses.join('.')
+        unless @klasses.empty?
+          result << parent
+          result << "."
+        end
+        result << name
+        return "#{result.join} = function#{args} {\n#{indent(body)}\n}"
+      else
+        raise "Unknown defn type: #{type1} for #{exp.inspect}"
+    end
   end
 
   def process_args(exp)
@@ -102,6 +99,20 @@ class Neko < SexpProcessor
   def process_scope(exp)
     exp.empty? ? "" : process(exp.shift)
   end
+
+  def process_if(exp)
+    c = process exp.shift
+    t = process exp.shift
+    f = process exp.shift
+
+    c = "(#{c.chomp})" if c =~ /\n/
+
+    r = "if #{c} {\n#{indent(t)}\n}\n"
+    r << "else {\n#{indent(f)}\n" if f
+    r << "}"
+    r
+  end
+
 
   def process_call(exp)
     receiver_node_type = exp.first.nil? ? nil : exp.first.first
@@ -137,21 +148,8 @@ class Neko < SexpProcessor
     end
   end
 
-  def process_str(exp)
-    return exp.shift.dump
-  end
-
-  def process_if(exp)
-    c = process exp.shift
-    t = process exp.shift
-    f = process exp.shift
-
-    c = "(#{c.chomp})" if c =~ /\n/
-
-    r = "if #{c} {\n#{t}}\n"
-    r << "else {\n#{f}\n" if f
-    r << "}"
-    r
+  def process_lvar(exp)
+    exp.shift.to_s
   end
 
   def process_arglist(exp)
@@ -162,35 +160,6 @@ class Neko < SexpProcessor
     code.join ', '
   end
 
-  def process_block(exp)
-    result = []
-
-    exp << nil if exp.empty?
-    until exp.empty? do
-      code = exp.shift
-      if code.nil? or code.first == :nil then
-        result << "# do nothing"
-      else
-        result << process(code)
-      end
-    end
-
-    result = result.join "\n"
-
-    result = case self.context[1]
-             when nil, :scope, :if, :iter, :resbody, :when, :while then
-               result + "\n"
-             else
-               "(#{result})"
-             end
-
-    return result
-  end
-
-  def process_lvar(exp)
-    exp.shift.to_s
-  end
-
   def process_lit(exp)
     exp.shift.inspect
   end
@@ -199,128 +168,24 @@ class Neko < SexpProcessor
     "return #{process(exp.shift)}"
   end
 
-  def process_lasgn(exp)
-    s = "var #{exp.shift}"
-    s += " = #{process exp.shift}" unless exp.empty?
-    s
-  end
-
-  def process_gasgn(exp)
-    s = "#{exp.shift.to_s.gsub('$', '')}"
-    s += " = #{process exp.shift}" unless exp.empty?
-    s
-  end
-
-  def process_gvar(exp)
-    return exp.shift.to_s.gsub('$', '')
-  end
-
-  def process_array(exp)
-    "$array(#{process_arglist(exp)})"
-  end
-
-  def process_const(exp)
-    exp.shift.to_s
-  end
-
-  def process_module(exp)
-    util_module_or_class(exp)
-  end
-
-  def process_class(exp)
-    util_module_or_class(exp, true)
-  end
-
-  def process_defn(exp)
-    type1 = exp[1].first
-    #type2 = exp[2] rescue nil #.first 
-
-    result = []
-
-    case type1
-        when :scope, :args then
-        name = exp.shift.to_s
-        name.gsub!('?', '_q')
-        args = process(exp.shift)
-        body = process(exp.shift)
-        parent = @klasses.join('.')
-        unless @klasses.empty?
-          result << parent
-          result << "."
-        end
-        result << name
-        return "#{result.join} = function#{args} {\n#{indent(body)}\n}"
-      else
-        raise "Unknown defn type: #{type1} for #{exp.inspect}"
-    end
-  end
-
-  def util_module_or_class(exp, is_class = false)
-    result = []
-
-    name = exp.shift
-    name = process name if Sexp === name
-
-    result << name
-    result << "="
-    result << "$new"
-
-    if is_class then
-      superk = process(exp.shift)
-      if superk == "Object" then
-        result << "(null);"
-      else
-        result << "(#{superk});"
-      end
-    else
-      result << "(null);"
-    end
-
-    result << "\n"
-
-    @klasses.push name
-
-    body = []
-    begin
-      code = process(exp.shift).chomp
-      body << code unless code.nil? or code.empty?
-    end until exp.empty?
-
-    unless body.empty? then
-      body = indent(body.join("\n\n") + "\n")
-    else
-      body = ""
-    end
-    result << body
-
-    @klasses.pop
-
-    result.join
-  end
-
-  def process_ivar(exp)
-    return "this.#{exp.shift.to_s.gsub("@", '')}"
-  end
-
-  def process_iasgn(exp)
-    lhs = exp.shift.to_s.gsub("@", '')
-    if exp.empty? then # part of an masgn
-      lhs.to_s
-    else
-      "this.#{lhs} = #{process exp.shift}"
-    end
-  end
-
-  def process_self(exp)
-    "this"
-  end
-
+  private
   def indent(s)
     s.to_s.split(/\n/).map{|line| @indent + line}.join("\n")
   end
 end
 
+code =<<-SCRIPT
+  def fib(n)
+    if n <= 1
+      return n
+    else
+      return fib(n-1) + fib(n-2)
+    end
+  end
+
+  puts fib(30)
+SCRIPT
+
 sexp = ParseTree.translate(code)
-puts "SEXP"
 pp sexp
-puts Neko.new.process(sexp)
+puts Neko.process(sexp)
